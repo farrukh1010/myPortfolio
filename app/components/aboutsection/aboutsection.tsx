16bb74ed3a4d61410e0b2845701cec7de68ea7af
farrukh1010 <42179299+farrukh1010@users.noreply.github.com>
2024-04-28 16:25:30 +0500

    minor changes

--- app/components/aboutsection/aboutsection.tsx
+++ app/components/aboutsection/aboutsection.tsx
@@ -2,12 +2,13 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import { tabDataTypes } from "@/types/types";
+import img from "./../../../public/images/main.jpg"
 
 const AboutSection = () => {
   return (
     <section className="" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 border border-double border-gray-500 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/main.jpg" width={500} height={500} alt="farrukh" />
+        <Image src={img} width={500} height={500} alt="farrukh" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-slate-600 mb-4">About Me</h2>
           <p className=" text-[#fffff] text-base sm:text-lg mb-6 lg:text-xl ">
@@ -18,8 +19,8 @@ const AboutSection = () => {
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
-          </div>
-          </div>
+        </div>
+      </div>
     </section>
   );
 };
