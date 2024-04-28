ca63a9f1391f0ffb213ed8fd4b8998cf3ca27af5
farrukh1010 <42179299+farrukh1010@users.noreply.github.com>
2024-04-28 15:59:27 +0500

    change in tage of image

--- app/components/aboutsection/aboutsection.tsx
+++ app/components/aboutsection/aboutsection.tsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
   return (
     <section className="" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 border border-double border-gray-500 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <img src="/images/main.jpg" width={500} height={500} alt="farrukh" />
+        <Image src="/images/main.jpg" width={500} height={500} alt="farrukh" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-slate-600 mb-4">About Me</h2>
           <p className=" text-[#fffff] text-base sm:text-lg mb-6 lg:text-xl ">
